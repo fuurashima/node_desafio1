16b1dc35eda298d3dcbb949b550ca2eedb4a2d40
Iván Cano Silva <icano@outlook.cl>
2024-11-25 23:36:17 -0300

    Update operaciones.js

--- operaciones.js
+++ operaciones.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const registrar = (nombre, edad, tipo, color, enfermedad) => {
     const nuevaCita = { nombre, edad, tipo, color, enfermedad };
 
-    // Leer el archivo citas.json
+    
     fs.readFile('citas.json', 'utf-8', (err, data) => {
         if (err) {
             console.error('Error leyendo el archivo:', err);
@@ -13,7 +13,7 @@ const registrar = (nombre, edad, tipo, color, enfermedad) => {
         const citas = JSON.parse(data);
         citas.push(nuevaCita);
 
-        // Guardar la nueva cita en el archivo
+        
         fs.writeFile('citas.json', JSON.stringify(citas, null, 2), (err) => {
             if (err) {
                 console.error('Error guardando la cita:', err);
@@ -25,7 +25,7 @@ const registrar = (nombre, edad, tipo, color, enfermedad) => {
 };
 
 const leer = () => {
-    // Leer el archivo citas.json
+    
     fs.readFile('citas.json', 'utf-8', (err, data) => {
         if (err) {
             console.error('Error leyendo el archivo:', err);
